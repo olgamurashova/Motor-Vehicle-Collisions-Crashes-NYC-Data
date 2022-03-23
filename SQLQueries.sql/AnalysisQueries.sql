bbcd5d86fc0a9c76b688ffcb5c74f066fd4696c8
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-23 15:01:44 -0400

    Update AnalysisQueries.sql

--- SQLQueries.sql/AnalysisQueries.sql
+++ SQLQueries.sql/AnalysisQueries.sql
@@ -11,3 +11,15 @@ SELECT COUNT(*)
 FROM public."Motor Vehicle Collisions"
 WHERE "CRASH DATE" BETWEEN '2021-01-01' AND '2021-12-31';
 
+3. Counting total number of injured persons, pedestrians, cyclists and motorists in 2021:
+
+
+
+
+SELECT "ZIP CODE", "BOROUGH", "LOCATION", "ON STREET NAME", "CROSS STREET NAME", "OFF STREET NAME",
+  COUNT("COLLISION_ID")
+FROM public."Motor Vehicle Collisions"
+WHERE "CRASH DATE" BETWEEN '2021-01-01' AND '2021-12-31' AND "ON STREET NAME" IS NOT NULL
+GROUP BY 1, 2, 3, 4,5, 6
+ORDER BY 7 DESC;
+
