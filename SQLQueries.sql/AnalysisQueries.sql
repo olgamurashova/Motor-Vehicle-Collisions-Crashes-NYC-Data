3dbd4f17c3be0e7b76f1cbf30fcc0f57f059665f
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-23 12:36:45 -0400

    Update AnalysisQueries.sql

--- SQLQueries.sql/AnalysisQueries.sql
+++ SQLQueries.sql/AnalysisQueries.sql
@@ -5,4 +5,9 @@ FROM public."Motor Vehicle Collisions"
 WHERE "CRASH DATE" BETWEEN '2021-01-01' AND '2021-12-31'
 ORDER BY "CRASH DATE" DESC;
 
-2. 
+2. Counting how many crashes and collisions occured in 2021:
+
+SELECT COUNT (*) 
+FROM public."Motor Vehicle Collisions"
+WHERE "CRASH DATE" BETWEEN '2021-01-01' AND '2021-12-31';
+
