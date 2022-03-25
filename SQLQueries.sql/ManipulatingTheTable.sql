b24f8820127643200a80d5417edea3de8e1fcbf8
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-25 14:47:00 -0400

    Update ManipulatingTheTable.sql

--- SQLQueries.sql/ManipulatingTheTable.sql
+++ SQLQueries.sql/ManipulatingTheTable.sql
@@ -5,3 +5,14 @@ COUNT(*) AS COUNT
 FROM public."Motor Vehicle Collisions"
 GROUP BY 1
 HAVING COUNT(*) > 1;
+
+2. Counting how many times a value occurs in a column:
+
+SELECT "BOROUGH",
+COUNT(*) AS COUNT
+FROM public."Motor Vehicle Collisions"
+WHERE "CRASH DATE" BETWEEN '2021-01-01' AND '2021-12-31'
+GROUP BY 1
+HAVING COUNT(*) > 1;
+
+3. 
